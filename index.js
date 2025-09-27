c366c7756b2cfe270597b5c6724a5e1868a15aea
Rohit Gaikwad <myselfrohitgaikwad@gmail.com>
2025-09-28 00:10:10 +0530

    Update index.js
    Now it will create a text filw were it will save the URL and its detials

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ inquirer
     const url=answers.URL;
     var qr_svg = qr.image(url, { type: 'png' });
     qr_svg.pipe(fs.createWriteStream('QRFile.png'));
+
+  fs.writeFile("URLdata.txt",url,(err) =>{
+      if (err){
+        throw err;
+      }
+      console.log("File Saved Succesfully");
+    });
+
   })
   .catch((error) => {
     if (error.isTtyError) {
